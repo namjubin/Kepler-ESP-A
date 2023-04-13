644454e9f71570b3cce676918dac949c44f20774
namjubin <113009247+namjubin@users.noreply.github.com>
2023-04-13 10:47:36 +0900

    Add files via upload

--- 7.ino
+++ 7.ino
@@ -1,48 +1,26 @@
 #define R 12
 #define G 13
 #define B 14
-#define btn 26
-
-int cnt = 0;
-bool push = false;
 
 void setup() {
   pinMode(R, OUTPUT);
   pinMode(G, OUTPUT);
   pinMode(B, OUTPUT);
-  pinMode(btn, INPUT);
 }
 
 void loop() {
-  
-  if(!digitalRead(btn)){
-    if(!push){
-      cnt = (cnt+1)%4;
-      push = true;
-    }
-  }
-  else{
-    push = false;
-  }
+  digitalWrite(R, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(B, LOW);
+  delay(1000);
 
-  if(cnt == 1){
-    digitalWrite(R, HIGH);
-    digitalWrite(G, LOW);
-    digitalWrite(B, LOW);
-  }
-  else if(cnt == 2){
-    digitalWrite(R, LOW);
-    digitalWrite(G, HIGH);
-    digitalWrite(B, LOW);
-  }
-  else if(cnt == 3){
-    digitalWrite(R, LOW);
-    digitalWrite(G, LOW);
-    digitalWrite(B, HIGH);
-  }
-  else{
-    digitalWrite(R, LOW);
-    digitalWrite(G, LOW);
-    digitalWrite(B, LOW);
-  }
+  digitalWrite(R, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(B, LOW);
+  delay(1000);
+
+  digitalWrite(R, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(B, HIGH);
+  delay(1000);
 }
